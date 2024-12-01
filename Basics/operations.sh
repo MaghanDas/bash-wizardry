0e6f4fe8cec1eb751c67450b8ccf444fe92e6c00
Maghan Das <dasmaghan360@gmail.com>
2024-12-01 12:32:19 +0100

    Basics of bash scripting

---
+++ Basics/operations.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+echo enter frist value $1
+echo enter second value$2
+echo addition : `expr $1 + $2`
+echo subtraction : `expr $1 - $2`
+echo Multiplication :`expr $1 * $2`
+echo Division L `expr $1 / $2`
+
